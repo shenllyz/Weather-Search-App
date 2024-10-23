79c802213bfd8ebb981a8fb81bcf454887530d69
shenllyz <102724218+shenllyz@users.noreply.github.com>
2024-10-22 21:04:32 -0700

    add mongoDB frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,19 @@
-const express = require('express');
+import express from "express";
+import cors from "cors";
+import { run } from './mongodb.js';
 const app = express();
 
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hello from App Engine!');
 });
 
+run().catch(console.dir);
+
+
+
 // Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
