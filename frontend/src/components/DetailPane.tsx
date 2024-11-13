ebb29f533dfd04fab84fa356825c59418b4caab1
shenllyz <102724218+shenllyz@users.noreply.github.com>
2024-11-13 12:37:26 -0800

    detail pane update

--- frontend/src/components/DetailPane.tsx
+++ frontend/src/components/DetailPane.tsx
@@ -1,57 +1,87 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
 import { getWeatherDescription, DailyWeather } from '../utils/weatherUtils';
-import { formatSunset, formatSunrise } from '../utils/formatDate';
+import {  formatDate, formatSunset, formatSunrise } from '../utils/formatDate';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 interface DetailPaneProps {
   weatherData: DailyWeather;
 }
 
 const DetailPane: React.FC<DetailPaneProps> = ({weatherData}) => {
   return (
-    <Table hover responsive>
-      <tbody className='text-start'>
-        <tr>
-          <th>Status</th>
-          <td>{getWeatherDescription(weatherData.weatherCode)}</td>
-        </tr>
-        <tr>
-          <th>Max Temperature</th>
-          <td>{weatherData.temperatureMax}°F</td>
-        </tr>
-        <tr>
-          <th>Min Temperature</th>
-          <td>{weatherData.temperatureMin}°F</td>
-        </tr>
-        <tr>
-          <th>Apparent Temperature</th>
-          <td>{weatherData.temperatureApparent}°F</td>
-        </tr>
-        <tr>
-          <th>Sun Rise Time</th>
-          <td>{formatSunrise(weatherData.sunriseTime)}</td>
-        </tr>
-        <tr>
-          <th>Sun Set Time</th>
-          <td>{formatSunset(weatherData.sunsetTime)}</td>
-        </tr>
-        <tr>
-          <th>Humidity</th>
-          <td>{weatherData.humidity}%</td>
-        </tr>
-        <tr>
-          <th>Wind Speed</th>
-          <td>{weatherData.windSpeed}mph</td>
-        </tr>
-        <tr>
-          <th>Visibility</th>
-          <td>{weatherData.visibility}mi</td>
-        </tr>
-        <tr>
-          <th>Cloud Cover</th>
-          <td>{weatherData.cloudCover}%</td>
-        </tr>
-      </tbody>
-    </Table>
+    <Container> 
+        <Row className='text-start mt-5'>
+            <Col xs={2}>
+                <Button  variant="outline-secondary" className="pe-3 ps-2 py-2 mb-4">
+                    <i><ArrowBackIosNewIcon/></i> 
+                    <span className='ms-2 fs-5'>List</span>
+                </Button>
+            </Col>
+            <Col xs={8} className='text-center'>
+                <h3 className='fw-bold'>{formatDate(weatherData.date)}</h3>
+            </Col>
+            <Col xs={2} className='text-end'>
+                <Button  variant="outline-secondary" className="px-3 py-1 mb-4">
+                <i className="bi bi-twitter-x fs-3"></i>
+                </Button>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <Table striped hover responsive>
+                <tbody className='text-start'>
+                    <tr>
+                    <th className="col-3 p-3">Status</th>
+                    <td className="col-8 p-3">{getWeatherDescription(weatherData.weatherCode)}</td>
+                    </tr>
+                    <tr>
+                    <th className="col-4 p-3">Max Temperature</th>
+                    <td className="col-8 p-3">{weatherData.temperatureMax}°F</td>
+                    </tr>
+                    <tr>
+                    <th className="col-4 p-3">Min Temperature</th>
+                    <td className="col-8 p-3">{weatherData.temperatureMin}°F</td>
+                    </tr>
+                    <tr>
+                    <th className="col-4 p-3">Apparent Temperature</th>
+                    <td className="col-8 p-3">{weatherData.temperatureApparent}°F</td>
+                    </tr>
+                    <tr>
+                    <th className="col-4 p-3">Sun Rise Time</th>
+                    <td className="col-8 p-3">{formatSunrise(weatherData.sunriseTime)}</td>
+                    </tr>
+                    <tr>
+                    <th className="col-4 p-3">Sun Set Time</th>
+                    <td className="col-8 p-3">{formatSunset(weatherData.sunsetTime)}</td>
+                    </tr>
+                    <tr>
+                    <th className="col-4 p-3">Humidity</th>
+                    <td className="col-8 p-3">{weatherData.humidity}%</td>
+                    </tr>
+                    <tr>
+                    <th className="col-4 p-3">Wind Speed</th>
+                    <td className="col-8 p-3">{weatherData.windSpeed}mph</td>
+                    </tr>
+                    <tr>
+                    <th className="col-4 p-3">Visibility</th>
+                    <td className="col-8 p-3">{weatherData.visibility}mi</td>
+                    </tr>
+                    <tr>
+                    <th className="col-4 p-3">Cloud Cover</th>
+                    <td className="col-8 p-3">{weatherData.cloudCover}%</td>
+                    </tr>
+                </tbody>
+                </Table>
+            </Col>
+        </Row> 
+        
+    </Container>
+
+     
   );
 };
 
--- frontend/src/components/Result.tsx
+++ frontend/src/components/Result.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'; 
-import { Container, Row, Col, Nav, Tab } from 'react-bootstrap';
+import {  Row, Col, Nav, Tab } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 import DailyView from './DailyView';
 import DailyTempChart from './DailyTempChart';
 import MeteogramComponent from './Meteogram';
@@ -8,6 +9,7 @@ import { DailyWeather, HourlyWeather } from '../utils/weatherUtils';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import FavoriteButton from './FavoriteButton';
 import DetailPane from './DetailPane';
+import Slide from '@mui/material/Slide';
 
 interface ResultProps {
   city: string;
@@ -42,59 +44,65 @@ const Result: React.FC<ResultProps> = ({ city, state, dailyWeatherData, hourlyWe
   const selectedWeather = dailyWeatherData[selectedWeatherIndex];
 
   return (
-    <Container>
-      <Row className='text-center mt-5'>
-        <Col>
-          <h2 className='result-title fs-3 fs-md-2'>Forecast at {city}, {state}</h2>
-        </Col>
-      </Row>
+    <Container className='px-0'>
+      <Slide direction="right" in={!showDetailPane} mountOnEnter unmountOnExit>
+        <div>
+          <Row className='text-center mt-5'>
+          <Col>
+            <h2 className='result-title fs-3 fs-md-2'>Forecast at {city}, {state}</h2>
+          </Col>
+        </Row>
 
-      <Row className=' mt-3'>
-        <Col className='text-end'>
-          <FavoriteButton 
-            onClick={handleFavoriteClick} 
-            isFavorite={isFavorite}
-          />
-          <a href="#" className='text-dark' onClick={handleDefaultDetailsClick}>
-            <span className='m-2'>Details<ArrowForwardIosIcon/></span>
-          </a>  
-        </Col>
-      </Row>
+        <Row className=' mt-3'>
+          <Col className='text-end'>
+            <FavoriteButton 
+              onClick={handleFavoriteClick} 
+              isFavorite={isFavorite}
+            />
+            <a href="#" className='text-dark' onClick={handleDefaultDetailsClick}>
+              <span className='m-2'>Details<ArrowForwardIosIcon/></span>
+            </a>  
+          </Col>
+        </Row>
 
-      <Row className='mt-4'>
-        <Col>
-          <Tab.Container activeKey={activeTab} onSelect={(k) => setActiveTab(k || 'dailyView')}>
-            <Nav variant="tabs" dir='rtl' className='px-0'>
-              <Nav.Item>
-                <Nav.Link eventKey="meteogram">Meteogram</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="dailyTempChart">Daily Temp. Chart</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="dailyView">Day View</Nav.Link>
-              </Nav.Item>
-            </Nav>
-            <Tab.Content className='mt-3'>
-              <Tab.Pane eventKey="meteogram">
-                <MeteogramComponent weatherData={hourlyWeatherData} />
-              </Tab.Pane>
-              <Tab.Pane eventKey="dailyTempChart">
-                <DailyTempChart weatherData={dailyWeatherData} />
-              </Tab.Pane>
-              <Tab.Pane eventKey="dailyView">
-                <DailyView weatherData={dailyWeatherData} onDetailsClick={handleDetailsClick} />
-              </Tab.Pane>
-            </Tab.Content>
-          </Tab.Container>
-        </Col>
-      </Row>
-
-      <div data-bs-toggle="offcanvas">
-        {showDetailPane && (
-          <DetailPane weatherData={selectedWeather} />
-        )}
-      </div>
+        <Row className='mt-4' id='res'>
+          <Col>
+            <Tab.Container activeKey={activeTab} onSelect={(k) => setActiveTab(k || 'dailyView')}>
+              <Nav variant="tabs" dir='rtl' className='px-0'>
+                <Nav.Item>
+                  <Nav.Link  eventKey="meteogram" href="#res">Meteogram</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="dailyTempChart" href="#res">Daily Temp. Chart</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="dailyView" href="#res">Day View</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content className='mt-3'>
+                <Tab.Pane eventKey="meteogram" unmountOnExit>
+                  <MeteogramComponent weatherData={hourlyWeatherData} />
+                </Tab.Pane>
+                <Tab.Pane eventKey="dailyTempChart" unmountOnExit>
+                  <DailyTempChart weatherData={dailyWeatherData} />
+                </Tab.Pane>
+                <Tab.Pane eventKey="dailyView" unmountOnExit>
+                  <DailyView weatherData={dailyWeatherData} onDetailsClick={handleDetailsClick} />
+                </Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+        </div>
+        
+      </Slide>
+      
+      <Slide direction="left" in={showDetailPane} mountOnEnter unmountOnExit timeout={500}>
+        <div>
+          <DetailPane weatherData={selectedWeather}/>
+        </div>
+           
+      </Slide>
     </Container>
   );
 };
